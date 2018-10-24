53dfb6267ddc5c4cd5d74c9213839f8915c1f73d
Eric K Wu <ekuan.wu@gmail.com>
2018-10-24 12:37:14 -0700

    Update site meta and title

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,21 +6,14 @@ import { StaticQuery, graphql } from 'gatsby'
 import '../assets/scss/main.scss'
 
 const Layout = ({ children, location }) => {
-
-  let content;
+  let content
 
   if (location && location.pathname === '/') {
-    content = (
-      <div>
-        {children}
-      </div>
-    )
+    content = <div>{children}</div>
   } else {
     content = (
       <div id="wrapper" className="page">
-        <div>
-          {children}
-        </div>
+        <div>{children}</div>
       </div>
     )
   }
@@ -41,8 +34,8 @@ const Layout = ({ children, location }) => {
           <Helmet
             title={data.site.siteMetadata.title}
             meta={[
-              { name: 'description', content: 'Sample' },
-              { name: 'keywords', content: 'sample, something' },
+              { name: 'description', content: 'Prism Dream Factory' },
+              { name: 'keywords', content: 'prism dream factory' },
             ]}
           >
             <html lang="en" />
